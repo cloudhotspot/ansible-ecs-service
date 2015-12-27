703424e4374d968bf75596137f9526ee480ca6ba
Justin Menga <justin.menga@gmail.com>
2015-12-27 01:27:30 -0800

    First release candidate

--- library/ecs_service.py
+++ library/ecs_service.py
@@ -38,15 +38,23 @@ options:
         required: False
     task_definition:
         description:
-            - The task definition family and optional revision of the service in the format family[:revision]. Required to create a new service.
+            - The task definition family and optional revision of the service in the format family[:revision] or ARN format. Required to create a new service.
         required: False
-    load_balancers:
+    load_balancer:
         description:
-            - The list of load balancer dictionary objects to access the service from.
+            - The ELB name or ARN to access the service from.  If configured, must be configured with role, container_name and container_port parameters.
+        required: False
+    container_name:
+        description:
+            - The task definition container name to access the service from the ELB.  If configured, must be configured with role, load_balancer and container_port parameters.
+        required: False
+     container_port:
+        description:
+            - The task definition container port to access the service from the ELB.  If configured, must be configured with role, load_balancer and container_name parameters.
         required: False
     role:
         description:
-            - The name or ARN of the IAM role that allows ECS to configure the load balancers.
+            - The IAM role name or ARN that allows ECS to configure the specified load_balancer.  If configured, must be configured with load_balancer, container_name and container_port parameters.
         required: False
     desired_count:
         description:
@@ -60,6 +68,10 @@ options:
         description:
             - Lower limit as percentage of desired_count (rounding down to nearest integer) of number of running healthy service instances that must be running during a deployment.
         required: False
+    wait_until_inactive:
+        description:
+            - When deleting a service, wait for service to reach an INACTIVE state.  When deleting a service, the service will first transition from an ACTIVE state to a DRAINING state, and then to an INACTIVE state when all client connections to the service have closed.  Default value is True. 
+        required: False
 '''
 
 EXAMPLES = '''
@@ -72,27 +84,57 @@ EXAMPLES = '''
     task_definition: console-sample-app-static-taskdef
     desired_count: 1
   register: service_output
-# Simple example of create service with load balancer
+# Simple example of create service with a load balancer. 
+# The role, load_balancer, container_name and container_port must be specified.
 - name: Create service with load balancer
   ecs_service:
       name: console-sample-app-service
       operation: create
       cluster: console-sample-app-static-cluster
       task_definition: console-sample-app-static-taskdef
-      load_balancers:
-        - name: my-elb
-          container_name: console-sample-app
-          container_port: 8000
+      role: ecsServiceRole
+      load_balancer: my-elb
+      container_name: console-sample-app
+      container_port: 8000
       desired_count: 2
       min_healthy_percent: 50
       max_percent: 200
   register: service_output
+# Simple example of updating a service
 - name: Update a service to use latest task definition revision
   ecs_service:
       name: console-sample-app-service
       operation: update
       cluster: console-sample-app-static-cluster
       task_definition: console-sample-app-static-taskdef
+# Simple example of updating a service to a specific task definition revision
+- name: Update a service to use specific task definition revision
+  ecs_service:
+      name: console-sample-app-service
+      operation: update
+      cluster: console-sample-app-static-cluster
+      task_definition: console-sample-app-static-taskdef:5
+# Simple example of changing the number of service instances
+- name: Update a service to use four instances
+  ecs_service:
+      name: console-sample-app-service
+      operation: update
+      cluster: console-sample-app-static-cluster
+      desired_count: 4
+# Simple example of deleting a service
+# The delete operation will change the desired count to 0 before deleting the service
+- name: Delete a service
+  ecs_service:
+      name: console-sample-app-service
+      operation: delete
+      cluster: console-sample-app-static-cluster
+# Simple example of deleting a service without waiting for the service to reach an INACTIVE state
+- name: Delete a service
+  ecs_service:
+      name: console-sample-app-service
+      operation: delete
+      cluster: console-sample-app-static-cluster
+      wait_until_inactive: false
 '''
 
 RETURN = '''
@@ -144,15 +186,20 @@ class EcsServiceManager:
         return None
 
 
-    def create_service(self, service_name, desired_count, task_definition, cluster_name='default', load_balancers=None, role=None, min_healthy_percent=None, max_percent=None):
+    def create_service(self, service_name, desired_count, task_definition, cluster_name='default', load_balancer=None, container_name=None, container_port=None, role=None, min_healthy_percent=None, max_percent=None):
         args = dict()
         deployment_config = dict()
+        load_balancers = dict()
         args['serviceName'] = service_name
         args['taskDefinition'] = task_definition
         args['desiredCount'] = desired_count
         args['cluster'] = cluster_name
-        if load_balancers:
-            args['loadBalancers'] = load_balancers
+        if load_balancer:
+            load_balancers['loadBalancerName'] = load_balancer
+        if container_name:
+            load_balancers['containerName'] = container_name
+        if container_port:
+            load_balancers['containerPort'] = container_port
         if role:
             args['role'] = role
         if min_healthy_percent:
@@ -161,6 +208,8 @@ class EcsServiceManager:
             deployment_config['maximumPercent'] = max_percent
         if deployment_config:
             args['deploymentConfiguration'] = deployment_config
+        if load_balancers:
+            args['loadBalancers'] = [load_balancers]
         try:
             response = self.ecs.create_service(**args)
         except Exception as e:
@@ -188,22 +237,40 @@ class EcsServiceManager:
             self.module.fail_json(msg="Can't update service - "+str(e))
         return response['service']
 
-    def delete_service(self, service_name, cluster_name='default'):
+    def delete_service(self, wait, service_name, cluster_name='default'):
         try:
-            waiter = self.ecs.get_waiter('services_inactive')
-
             # Set service desired count to zero
             response = self.update_service(service_name, 0, cluster_name)
 
             # Delete service
             self.ecs.delete_service(cluster=cluster_name, service=service_name)
 
-            # Wait for service to become inactive
-            waiter.wait(cluster=cluster_name, services=[ service_name ])
+            if wait:
+                # Wait for service to become inactive
+                waiter = self.ecs.get_waiter('services_inactive')
+                waiter.wait(cluster=cluster_name, services=[ service_name ])
+            
             response = self.describe_services(cluster_name, service_name)
         except Exception as e:
             self.module.fail_json(msg="Can't delete service - "+str(e))
         return response
+
+    def check_for_update(self, desired, existing):
+        """Compares desired state with existing state to determine any changes required"""
+        # Construct target state
+        target=dict()
+        target_deployment_config=dict()
+        existing_deployment_config=existing.get('deploymentConfiguration')
+        if desired.get('desired_count'):
+            target['desiredCount'] = desired.get('desired_count')
+        if desired.get('task_definition'):
+            target['taskDefinition'] = desired.get('task_definition')
+        if desired.get('min_healthy_percent'):
+            target_deployment_config['minimumHealthyPercent'] = desired.get('min_healthy_percent')
+        if desired.get('max_percent'):
+            target_deployment_config['maximumPercent'] = desired.get('max_percent')
+        return [item for item in target.items() if item not in existing.items()] or \
+               [item for item in target_deployment_config.items() if item not in existing_deployment_config.items()]
 
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
@@ -213,24 +280,8 @@ def json_serial(obj):
     raise TypeError ("Type not serializable")
 
 def fix_datetime(result):
-    """Temporary fix to convert datetime fields from Boto3 to dateiime string"""
+    """Temporary fix to convert datetime fields from Boto3 to dateiime string.  See https://github.com/ansible/ansible-modules-extras/issues/1348."""
     return json.loads(json.dumps(result, default=json_serial))
-
-def check_for_update(desired, existing):
-    """Compares desired state with existing state to determine any changes required"""
-    # Construct target state
-    target=dict()
-    deployments=dict()
-    if desired.get('desired_count'):
-        target['desiredCount'] = desired.get('desired_count')
-    if desired.get('min_healthy_percent'):
-        deployments['minimumHealthyPercent'] = desired.get('min_healthy_percent')
-    if desired.get('max_percent'):
-        deployments['maximumPercent'] = desired.get('max_percent')
-    if deployments:
-        target['deployments'] = deployments
-    # Check if target state differs from existing state
-    return [item for item in target.items() if item not in existing.items()]
 
 def main():
     argument_spec = ec2_argument_spec()
@@ -239,11 +290,14 @@ def main():
         operation=dict(required=True, choices=['create', 'update', 'delete']),
         cluster=dict(required=False, type='str'),
         task_definition=dict(required=False, type='str' ), 
-        load_balancers=dict(required=False, type='list'),
+        load_balancer=dict(required=False, type='str'),
+        container_name=dict(required=False, type='str'),
+        container_port=dict(required=False, type='int'),
         role=dict(required=False, type='str'),
         desired_count=dict(required=False, type='int'),
         min_healthy_percent=dict(required=False, type='int'),
-        max_percent=dict(required=False, type='int')
+        max_percent=dict(required=False, type='int'),
+        wait_until_inactive=dict(default=True, required=False, choices=BOOLEANS)
     ))
 
     module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
@@ -261,6 +315,9 @@ def main():
             module.fail_json(msg="To create a service, the desired_count must be specified")
         if not 'task_definition' in module.params and module.params['task_definition'] is None:
             module.fail_json(msg="To create a service, a task_definition must be specified")
+        if not ((module.params['load_balancer'] is None) == (module.params['role'] is None) == \
+               (module.params['container_port'] is None) == (module.params['container_name'] is None)):
+            module.fail_json(msg="When configuring load_balancer, container_name, container_port or role - you must specify load_balancer, container_name, container_port and role")
 
     # Get existing service
     service_mgr = EcsServiceManager(module)
@@ -272,16 +329,14 @@ def main():
             results['service'] = fix_datetime(existing)
         else:
             if not module.check_mode:
-                if module.params['load_balancers']:
-                    service_params['loadBalancers'] = module.params['load_balancers']
-                if module.params['role']:
-                    service_params['role'] = module.params['role']
                 results['service'] = fix_datetime(service_mgr.create_service(
                     module.params.get('name'),
                     module.params.get('desired_count'),    
                     module.params.get('task_definition'),
                     module.params.get('cluster'),
-                    module.params.get('load_balancers'),
+                    module.params.get('load_balancer'),
+                    module.params.get('container_name'),
+                    module.params.get('container_port'),
                     module.params.get('role'),
                     module.params.get('min_healthy_percent'),
                     module.params.get('max_percent') 
@@ -291,7 +346,7 @@ def main():
     elif module.params['operation'] == 'update':
         if not existing:
             module.fail_json(msg="Service to update was not found")
-        elif check_for_update(module.params, existing):
+        elif service_mgr.check_for_update(module.params, existing):
             if not module.check_mode:
                 results['service'] = fix_datetime(service_mgr.update_service(
                     module.params.get('name'),
@@ -311,6 +366,7 @@ def main():
             # it exists, so we should delete it and mark changed.
             # return info about the cluster deleted
                 results['service'] = fix_datetime(service_mgr.delete_service(
+                    module.params['wait_until_inactive'],
                     module.params['name'],
                     module.params['cluster']
                 ))
