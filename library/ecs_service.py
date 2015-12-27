ac21f9d00d2715238adf55afccef31454ce8e22b
Justin Menga <justin.menga@gmail.com>
2015-12-27 01:28:31 -0800

    Add Github ID

--- library/ecs_service.py
+++ library/ecs_service.py
@@ -21,7 +21,7 @@ short_description: create, update or delete a service in ecs
 description:
     - Creates, updates or deletes ecs services.
 version_added: "2.0"
-author: Justin Menga(justin.menga@gmail.com)
+author: Justin Menga(@mixja)
 options:
     name: 
         description:
